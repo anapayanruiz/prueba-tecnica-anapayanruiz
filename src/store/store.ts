8ab4ad7a41f398e6deb4f846ba783ffe4e4867ad
anapayanruiz <ana.payan@stickercontrol.com>
2023-02-11 14:52:20 +0100

    chore: add messages store module

---
+++ src/store/store.ts
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex';
+import Messages from './modules/messages';
+
+export default createStore({
+  state: {
+  },
+  modules: {
+    messages: Messages,
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  getters: {
+  }
+});
